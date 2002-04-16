da682d29459e5c959f384769e26ec2059a309a7b
Rainer Clasen <bj@zuto.de>
2002-04-16 17:39:28 +0000

    new defaults for db

--- opt.c
+++ opt.c
@@ -5,7 +5,7 @@ char *opt_player = "/home/bj/.mserv/player";
 char *opt_db_host = "";
 char *opt_db_port = "";
 char *opt_db_name = "dudl";
-char *opt_db_user = "reader";
-char *opt_db_pass = "reader";
+char *opt_db_user = "mserv";
+char *opt_db_pass = "mserv";
 
 
